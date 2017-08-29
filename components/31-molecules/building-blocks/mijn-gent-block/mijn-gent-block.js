1e5af7735b8224f658789163c6c76a75f36bb040
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2017-08-29 15:08:19 +0200

    STIJ-4: Added Mijn Gent component.

---
+++ components/31-molecules/building-blocks/mijn-gent-block/mijn-gent-block.js
@@ -0,0 +1,20 @@
+(function ($) {
+  'use strict';
+
+  /**
+   * Invoked after after loading the initial page and after each AJAX request.
+   *
+   * @param context
+   *   The DOM context.
+   * @param settings
+   *   Object of additional settings.
+   */
+  $(window).on('load', loadMijnGentBlock);
+
+  /**
+   * Replace the hamburger menu.
+   */
+  function loadMijnGentBlock() {
+  }
+
+})(jQuery);
