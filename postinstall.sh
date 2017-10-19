7232344ba02e16ab0321fb8b9f4896cce8e1a7a8
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2017-10-19 14:13:35 +0200

    Removed ugly error log and added fix to postinstall script.

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf ./public/styleguide/vendor;
+
+mkdir ./public/styleguide/vendor;
+
 cp -R node_modules/lightgallery ./public/styleguide/vendor/lightgallery;
 cp -R node_modules/jquery ./public/styleguide/vendor/jquery;
 cp -R node_modules/chosen-js ./public/styleguide/vendor/chosen-js;
