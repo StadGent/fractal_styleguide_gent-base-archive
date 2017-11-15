998a96486cd4842bc015f3efb860563ef46ebf90
bart <bart.delrue@digipolis.gent>
2017-11-15 14:13:03 +0100

    STIJ-148: moved TabTrap to global 'gent' variable.

--- components/31-molecules/building-blocks/hamburger-menu/hamburger-menu.functions.js
+++ components/31-molecules/building-blocks/hamburger-menu/hamburger-menu.functions.js
@@ -22,62 +22,13 @@
       var closeBtn = hamburgerMenu.find('.close');
       var overlay = $('.hamburger-menu-overlay');
       var trigger;
-      var tabTrap = new TabTrap(this[0]);
 
-      /**
-       * Generates a tabTrap object
-       *
-       * @param {object} container DOM-element
-       * @constructor
-       */
-      function TabTrap(container) {
-        var focusPosition = -1;
-        var focusables = getFocusables(container);
-
-        /**
-         * Returns all focusable elements within a given container.
-         *
-         * @param {object} container hamburger DOM-element
-         * @return {array} focusable elements
-         */
-        function getFocusables(container) {
-          var focusables = container
-            .querySelectorAll('a[href], ' +
-              'area[href], ' +
-              'input:not([disabled]):not([hidden]), ' +
-              'select:not([disabled]), ' +
-              'textarea:not([disabled]), ' +
-              'button:not([disabled]), ' +
-              '[tabindex="0"]');
-          return Array.prototype.slice.call(focusables);
-        }
-
-        this.next = function () {
-          if (++focusPosition > focusables.length - 1) {
-            focusPosition = 0;
-          }
-          focusables[focusPosition].focus();
-        };
-
-        this.back = function () {
-          if (--focusPosition < 0) {
-            focusPosition = focusables.length - 1;
-          }
-          focusables[focusPosition].focus();
-        };
-
-        this.home = function () {
-          focusPosition = 0;
-          focusables[focusPosition].focus();
-        };
-
-        this.end = function () {
-          focusPosition = focusables.length - 1;
-          focusables[focusPosition].focus();
-        };
-
-        this.hasFocusables = focusables && focusables.length > 0;
+      if(typeof gent === 'undefined') {
+        console.error('You need to include base.js.');
+        return;
       }
+
+      var tabTrap = new gent.TabTrap(this[0]);
 
       /**
        * Closes the hamburger menu
