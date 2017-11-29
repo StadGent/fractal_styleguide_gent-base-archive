c29f2350409390843a6a0f3c8e5edb4c72aa519d
Jeroen Goossens <jeroen@pau.be>
2017-11-29 15:32:06 +0100

    OPEN-57: Refactored opening hours js lib.

--- components/21-atoms/openinghours/openinghours.js
+++ components/21-atoms/openinghours/openinghours.js
@@ -1,72 +1,98 @@
-/**
- * @file
- * Implements Ghent's opening hours widgets.
- *
- * @author
- * Jeroen Goossens
- *
- */
-(function ($) {
+window.openingHours = (function () {
   'use strict';
 
-  $.fn.openingHours = function (options) {
-    function _getDate(date) {
-      return date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
+  var defaults = {
+    endpoint: 'https://openingsuren.gentgrp.gent.be'
+  };
+
+  function OpeningHours(items, options) {
+    this.s = Object.assign({}, defaults, options);
+    this.items = items;
+
+    for (var i = 0; i < items.length; i++) {
+      this._current = items[i];
+      this.init(items[i]);
     }
 
-    var now = new Date();
-    var defaults = {
-      endpoint: '//openingsuren.stad.gent'
-    };
-    options = $.extend(defaults, options);
+    return this;
+  }
 
-    $(this).each(function (i) {
-      function _getOpeningHours(element) {
-        if (typeof element.data('service') !== 'number' || typeof element.data('channel') !== 'number') {
-          element.html('Error: Please provide a service and channel.');
-          return null;
-        }
+  OpeningHours.prototype.init = function () {
+    if (isNaN(this._current.dataset.service) || isNaN(this._current.dataset.channel)) {
+      this._current.innerHTML = 'Error: Please provide a service and channel.';
+      return false;
+    }
 
-        if (typeof element.data('date') === 'string' && element.data('type') === 'open-now') {
-          element.html('Error: Date is not supported in the "open now" widget.');
-          return null;
-        }
+    if (typeof this._current.dataset.date === 'string' && this._current.dataset.type === 'open-now') {
+      this._current.innerHTML = 'Error: Date is not supported in the "open now" widget.';
+      return false;
+    }
 
-        var date;
-        if (typeof element.data('date') !== 'string') {
-          date = new Date();
-        }
-        else {
-          date = new Date(element.data('date'));
-        }
-        date = _getDate(date);
+    if (typeof this._current.dataset.date === 'undefined') {
+      this._current.dataset.date = new Date().toUTCString();
+    }
 
-        switch (element.data('type')) {
-          case 'open-now':
-            _request(options.endpoint + '/api/v1/services/' + element.data('service') + '/channels/' + element.data('channel') + '/open-now');
-            break;
-          case 'month':
-            _request(options.endpoint + '/api/v1/services/' + element.data('service') + '/channels/' + element.data('channel') + '/openinghours/month?date=' + date);
-            break;
-          default:
-            var until = new Date();
-            until.setDate(now.getDate() + 6);
-            _request(options.endpoint + '/api/v1/services/' + element.data('service') + '/channels/' + element.data('channel') + '/openinghours?from=' + date + '&until=' + _getDate(until));
-            break;
-        }
-      }
-      function _request(request) {
-        $.ajax({
-          url: request,
-          dataType: 'html',
-          success: function (html) {
-            element.html(html);
-          }
-        });
-      }
-
-      var element = $(this);
-      _getOpeningHours(element);
-    });
+    var url = this.constructRequest();
+    this.request(url, this.print);
   };
-}(jQuery));
+
+  OpeningHours.prototype.formattedDate = function (d) {
+    var date;
+    if (!d) {
+      date = new Date();
+    }
+    else {
+      date = new Date(d);
+    }
+
+    return date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
+  };
+
+  OpeningHours.prototype.constructRequest = function () {
+    switch (this._current.dataset.type) {
+      case 'open-now':
+        return this.s.endpoint + '/api/v1/services/' + this._current.dataset.service + '/channels/' + this._current.dataset.channel + '/open-now';
+      case 'month':
+        return this.s.endpoint + '/api/v1/services/' + this._current.dataset.service + '/channels/' + this._current.dataset.channel + '/openinghours/month?date=' + this.formattedDate(this._current.dataset.date);
+      default:
+        var until = new Date(this._current.dataset.date);
+        until.setDate(until.getDate() + 6);
+        return this.s.endpoint + '/api/v1/services/' + this._current.dataset.service + '/channels/' + this._current.dataset.channel + '/openinghours?from=' + this.formattedDate(this._current.dataset.date) + '&until=' + this.formattedDate(until);
+    }
+  };
+
+  OpeningHours.prototype.request = function (url, callback) {
+    var xmlhttp;
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.element = this._current;
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+        callback(xmlhttp.element, xmlhttp.responseText);
+      }
+    };
+    xmlhttp.open('GET', url, true);
+    xmlhttp.setRequestHeader('Accept', 'text/html');
+    xmlhttp.send();
+  };
+
+  OpeningHours.prototype.print = function (element, data) {
+    element.innerHTML = data;
+  };
+
+  var openinghours = {
+    load: function (options) {
+      var items = document.querySelectorAll('.openinghours');
+      new OpeningHours(items, options);
+    }
+  };
+
+  return openinghours;
+}());
+
+document.onreadystatechange = function () {
+  'use strict';
+
+  if (document.readyState === 'interactive') {
+    window.openingHours.load();
+  }
+};
