214322b25d68790d8dbebba2b9106fbded7d598c
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2017-12-07 13:39:04 +0100

    STIJ-208: Added generation of main_cli.scss to the postinstall script.

--- postinstall.sh
+++ postinstall.sh
@@ -9,3 +9,24 @@ if [ -d "./public" ]; then
   cp -R node_modules/jquery ./public/styleguide/vendor/jquery;
   cp -R node_modules/chosen-js ./public/styleguide/vendor/chosen-js;
 fi
+
+touch components/main_cli.scss
+
+echo "// inject:settings
+// endinject
+
+// inject:mixins
+// endinject
+
+// inject:base
+// endinject
+
+// inject:atoms
+// endinject
+
+// inject:molecules
+// endinject
+
+// inject:organisms
+// endinject
+" >>  components/main_cli.scss
