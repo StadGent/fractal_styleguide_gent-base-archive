7fd1d33ad12fad926c9ed6144dd0b6b742f88811
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2017-12-08 15:57:41 +0100

    STIJ-208: Fixed typo in docs.

--- postinstall.sh
+++ postinstall.sh
@@ -16,7 +16,7 @@ fi
 #
 # This step is only used by Digipolis while building the style guide
 # When downloading the NPM package and running npm install or yarn install the "components" directory
-# does not exist, so we don' have to create this file here.
+# does not exist, so we don't have to create this file here.
 #
 if [ -d "./components" ]; then
   if [ -f components/main_cli.scss ]; then
