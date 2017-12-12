83c75911d06f7f0e1ac169828535635b768f4e26
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2017-12-12 10:58:14 +0100

    STIJ-200: Added documentation to the postinstall.sh script.

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+#===============================================================================
+#
+#          FILE:  postinstall.sh
+#
+#         USAGE:  ./postinstall.sh --type=patch
+#
+#   DESCRIPTION: Depending on when the postinstall.sh script is being run:
+#                - After running "yarn install" when cloning the style guide repo to your local system.
+#                   (used by people working on the style guide).
+#                  It checks for the "components" directory and "main_cli.scss" file. If it can't find it it creates
+#                  one and injects the necessary tags for gulp to inject the necessary SASS partials.
+#
+#                - After running "npm install" or "yarn install" WITHOUT cloning to your local system.
+#                   (used by people implementing the style guide).
+#                  If checks for the "public" directory and creates the "public/styleguide/vendor" directory.
+#                  It then adds some node_module packages to this directory needed for implementing the style guide.
+#
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Gert-Jan Meire, gertjan.meire@digipolis.gent
+#       COMPANY:  Digipolis Gent
+#       VERSION:  1.0
+#       CREATED:  12/12/2017
+#      REVISION:  ---
+#===============================================================================
+
 #
 # Create the necessary directories and move files to them after npm install or yarn install is run.
 #
