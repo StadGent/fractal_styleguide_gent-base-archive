48becc2ec7a546e4b0a6dbb87fd52fa7a6bc219a
daften <dieterblomme@gmail.com>
2018-01-19 16:58:01 +0100

    Disabled MD033 for inline HTML.
    Has to be done until https://github.com/markdownlint/markdownlint/issues/189 is fixed.

--- .mdl.style.rb
+++ .mdl.style.rb
@@ -5,4 +5,5 @@ rule 'MD029', :style => "ordered"
 
 exclude_rule "MD024"
 exclude_rule "MD028"
+exclude_rule "MD033" # Until https://github.com/markdownlint/markdownlint/issues/189 is fixed 
 exclude_rule "MD041"
